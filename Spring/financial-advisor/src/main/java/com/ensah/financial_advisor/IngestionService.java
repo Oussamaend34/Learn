6ef71508a2478563ab7f686cf7cbdd225cdf10c1
OUSSAMA EL ALAOUI EL ISMAILI <oussama.elalaouielismali@etu.uae.ac.ma>
2025-02-22 21:57:03 +0100

    fixing ingestionService

--- Spring/financial-advisor/src/main/java/com/ensah/financial_advisor/IngestionService.java
+++ Spring/financial-advisor/src/main/java/com/ensah/financial_advisor/IngestionService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Component;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Component
 @Slf4j
 @RequiredArgsConstructor
 public class IngestionService implements CommandLineRunner{

5624d74212d6f39af66548c1591a339fa400fa96
OUSSAMA EL ALAOUI EL ISMAILI <oussama.elalaouielismali@etu.uae.ac.ma>
2025-02-27 23:01:41 +0000

    adding delete and update posts endpoints

--- FastAPI/CRUDTutorial/main.py
+++ FastAPI/CRUDTutorial/main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange 
@@ -22,6 +22,10 @@ def find_post(id):
     for p in my_posts:
         if p["id"] == id:
             return p
+def find_post_index(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
 
 @app.get("/")
 def root():
@@ -31,7 +35,7 @@ def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code = status.HTTP_201_CREATED)
 def create_post(post: Post):
     print(post.model_dump())
     post_dic = post.model_dump()  
@@ -39,7 +43,28 @@ def create_post(post: Post):
     my_posts.append(post_dic)
     return {"data": post_dic}
 
+@app.get("/posts/latest")
+def get_latest_post():
+    post = my_posts[-1]
+    return {"data": post} 
+
 @app.get("/posts/{id}")
-def get_post_by_id(id:int):
-    post = find_post(id=id)  
+def get_post_by_id(id:int): 
+    post = find_post(id=id) 
+    if not post:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Post with id {id} not found"
+            )
     return {"data": post}
+
+@app.delete("/posts/{id}", status_code = status.HTTP_204_NO_CONTENT)
+def delete_post_by_id(id:int):
+    index = find_post_index(id=id)
+    if index is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Post with id {id} not found"
+            )
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
